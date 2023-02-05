7898d2787bbdd70a3438b304cc6a1ca37e7603e3
João <joaovictorpfr@gmail.com>
2023-02-05 19:13:41 -0300

    Cover header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,66 +1,75 @@
-import { border } from '@mui/system';
-import { styled } from '../../../stitches.config'
+import { border } from "@mui/system";
+import { styled } from "../../../stitches.config";
 
-export function Header(){
-    return (
-      <Background>
-        <Nav>
-          <MenuHam>
-            <img src="../../../src/assets/icons/menu.svg" alt="" />
-          </MenuHam>
-          <img
-            height="120"
-            src="../../../src/assets/imgs/bankPeople.svg"
-            alt=""
-          />
-          <Texts>
-            <Title>Ito Bank</Title>
-            <Desc>O banco para você enriquecer!</Desc>
-          </Texts>
-        </Nav>
+export function Header() {
+  return (
+    <Background>
+      <Nav>
+        <MenuHam>
+          <img src="../../../src/assets/icons/menu.svg" alt="" />
+        </MenuHam>
+        <img
+          height="120"
+          src="../../../src/assets/imgs/bankPeople.svg"
+          alt=""
+        />
+        <Texts>
+          <Title>Ito Bank</Title>
+          <Desc>O banco para você enriquecer!</Desc>
+        </Texts>
+      </Nav>
+      <Cover>
+        <img height="500" src="../../../src/assets/imgs/home.svg" alt="" />
+        <Flan>
+            <Legend>
+                Nós somos a solução!
+            </Legend>
 
-        
-      </Background>
-    );
+            <Invest>
+                Investir Agora!
+            </Invest>
+        </Flan>
+      </Cover>
+    </Background>
+  );
 }
 
 const Background = styled("header", {
-  display: 'flex',  
   height: "90vh",
   width: "100vw",
-  backgroundColor: '$aquaWhite',
+  backgroundColor: "$aquaWhite",
 });
 
 const Nav = styled("nav", {
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: "10%",
-    width: "100%",
-    backgroundColor: "$blue400",
-    margin: '0',
-    padding: '30px'
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  height: "10%",
+  width: "100%",
+  backgroundColor: "$blue400",
+  margin: "0",
+  padding: "30px",
 });
 
-const MenuHam = styled('button', {
-    backgroundColor: '$blue400',
-    border: 'none',
+const MenuHam = styled("button", {
+  backgroundColor: "$blue400",
+  border: "none",
 });
 
-const Texts = styled('div', {
-    display: 'flex', 
-    flexDirection: 'column', 
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%'
+const Texts = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
 });
 
 const Title = styled("h2", {
-    fontSize: '2.5rem',
-    fontFamily: '$Noto',
-    fontWeight: 'bold',
-    color: '$aquaGrey',
-    textShadow: '3px 3px 3px #6342f7'
+  fontSize: "2.5rem",
+  fontFamily: "$Noto",
+  fontWeight: "bold",
+  color: "$aquaGrey",
+  textShadow: "3px 3px 3px #6342f7",
 });
 
 const Desc = styled("span", {
@@ -69,4 +78,39 @@ const Desc = styled("span", {
   fontWeight: "bold",
   color: "$aquaGrey",
   textShadow: "3px 3px 3px #6342f7",
+});
+
+const Cover = styled('div', {
+    height: '90%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+});
+
+const Flan = styled('div', {
+    height: '20%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    padding: '10px'
+});
+
+const Legend = styled('legend', {
+    fontSize: '3.2rem',
+    fontFamily: '$Poppins'
+});
+
+const Invest = styled('button', {
+    fontSize: '1.6rem',
+    fontFamily: '$Poppins',
+    fontWeight: 'bold',
+    height: '60px',
+    width: '80%',
+    border: 'none',
+    borderRadius: '10px',
+    backgroundColor: '$blue400',
+    color: '#fff',
+    boxShadow: '5px 5px 5px #584dff'
 });
