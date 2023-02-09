7cda3f82f1e24fe28bff9f0a1fca1c3de1321bfe
João <joaovictorpfr@gmail.com>
2023-02-09 17:24:03 -0300

    Add base styles and props information, input forms

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,20 @@
 import { styled } from "../../../stitches.config";
+import { Input } from '../Form/input'
 
 export function Form() {
   return (
-    <Card>
-      <h1>Hello World!</h1>
-    </Card>
+    <Background>
+      <Card>
+        <Title>Cadastre-se: </Title>
+        <Input 
+        type="text"
+        />
+      </Card>
+    </Background>
   );
 }
 
-const Card = styled("div", {
+const Background = styled("section", {
   display: "flex",
   alignItems: "center",
   justifyContent: "space-evenly",
@@ -17,4 +23,25 @@ const Card = styled("div", {
   backgroundColor: "$aquaWhite",
   boxShadow: "8px 8px 8px #cacaca",
   marginBottom: "50px",
+});
+
+const Card = styled("div", {
+  height: "90%",
+  width: "70%",
+  borderRadius: "10px",
+  boxShadow: "-8px 8px 8px 8px #6278F7",
+  border: "5px solid #96a5fd",
+  display: "flex",
+  flexDirection: 'column',
+  alignItems: "center",
+  justifyContent: "space-evenly",
+  backgroundColor: "#fff",
+});
+
+const Title = styled("h2", {
+  fontSize: "2.7rem",
+  fontFamily: "$Poppins",
+  fontWeight: "bold",
+  color: "$lightBlack",
+  textShadow: "3px 3px 3px #a7a7a7",
 });
--- src/components/Form/input.jsx
+++ src/components/Form/input.jsx
@@ -1,9 +1,34 @@
+import { fontWeight } from "@mui/system";
 import { styled } from "../../../stitches.config";
 
-export function Input(){
-    return(
-        <Input>
-        
-        </Input>
-    )
+export function Input(props)  {
+
+    const typing = (event) => {
+      props.onChanges(event.target.value);
+    };
+
+  return (
+    <>
+      <Label>{props.label}</Label>
+      <InputLabel
+        value={props.value}
+        onChange={typing}
+        required={props.required}
+        type={props.type}
+        placeholder={props.placeholder}
+      ></InputLabel>
+    </>
+  );
 }
+
+const Label = styled('label', {
+    fontSize: '2rem', 
+    fontFamily: '$Inter',
+    fontWeight: 'bold',
+})
+
+const InputLabel = styled("input", {
+  height: '30px',
+  width: '80%',
+  padding: '10px',
+});
